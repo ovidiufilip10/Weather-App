ed081088be9c8f5827a3e0d5c9c5475fc76b3653
ovidiufilip10 <ovidiufilip10@yahoo.com>
2023-03-22 10:31:50 +0200

    Adding the components

--- weather-app-react/src/App.jsx
+++ weather-app-react/src/App.jsx
@@ -1,6 +1,21 @@
-import { useState } from 'react'
+import SelectedDay from "./components/SelectedDay"
+import WeekCard from "./components/WeekCard"
 import './App.css'
 
 function App() {
+return (
+  <div>
+      <SelectedDay/>
+      <section>
+        <WeekCard/>
+        <WeekCard/>
+        <WeekCard/>
+        <WeekCard/>
+        <WeekCard/>
+        <WeekCard/>
+        <WeekCard/>
+      </section>
+  </div>
+)
 }
 export default App
